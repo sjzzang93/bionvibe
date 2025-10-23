ae0d45b04c860002b701f7c2c208e1721b609283
sjzzang93 <sfdpceo@gmail.com>
2025-10-23 23:25:25 +0900

    remove: 시력테스트 앱 삭제

--- app/apps/eye-test/components/Disclaimer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Disclaimer() {
-  return (
-    <p className="text-xs text-neutral-500 leading-relaxed px-3">
-      ※ 본 웹앱은 참고용 자가 검사 도구이며 <b>의료 진단이 아닙니다</b>. 이상이
-      의심되면 반드시 <b>안과 전문의</b> 진료를 받으세요.
-    </p>
-  );
-}
-
